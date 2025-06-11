d0f27fe9bea204a1ed60401156893466f8029ed9
jayant <jayantbca012@gmail.com>
2025-06-11 13:30:39 +0530

    this is my first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOTE= '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+    if HYPEN_E_DOTE in requirements:
+        requirements.remove(HYPEN_E_DOTE)
+    return requirements
+    
+
+setup (
+  name='Fault deetction',
+  version='0.1',
+  author='jayant',
+  author_mail='jayantbca012@gmail.com',
+  install_reqirements=get_requirements('requirements.txt'),
+  packages=find_packages()
+)
